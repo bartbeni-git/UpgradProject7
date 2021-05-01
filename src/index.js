36cb88bb62bd22982a636fa84747f63595d18bfb
bartbeni <bartbeni@gmail.com>
2021-05-01 14:52:17 +0530

    Installed Material,Typeface,Router,Dom. Header WIP

--- src/index.js
+++ src/index.js
@@ -1,10 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
+import App from './App'
+import 'fontsource-roboto';
 
-ReactDOM.render(
-    <span>
-        Image Viewer
-    </span>, 
-    document.getElementById('root')
-);
+
+ReactDOM.render(<App/>, document.getElementById('root'));
